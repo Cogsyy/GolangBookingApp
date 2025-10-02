e18a5f16cace5904dc8926abdcf09de04dfc9c56
Jordan <jordancog94@gmail.com>
2025-10-02 10:20:08 -0400

    Adding work done so far to git. Basic data entry for booking app coded following along the Youtube course

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	conferenceName := "Go conference"
+	const conferenceTickets = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
+	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend.")
+
+	var bookings [50]string
+	bookings[0] = "A"
+	bookings[1] = "B"
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	//Ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+}
